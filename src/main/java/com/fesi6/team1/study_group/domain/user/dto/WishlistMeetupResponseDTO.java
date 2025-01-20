aa66a56d93479ccf557619a98c1e91b527a8b1fc
kkh <keeff1@naver.com>
2025-01-20 15:59:48 +0900

    찜한 모임 정보 DTO 수정

--- src/main/java/com/fesi6/team1/study_group/domain/user/dto/WishlistMeetupResponseDTO.java
+++ src/main/java/com/fesi6/team1/study_group/domain/user/dto/WishlistMeetupResponseDTO.java
@@ -1,5 +1,6 @@
 package com.fesi6.team1.study_group.domain.user.dto;
 
+import com.fesi6.team1.study_group.domain.meetup.dto.ParticipantDTO;
 import com.fesi6.team1.study_group.domain.meetup.entity.MeetingType;
 import com.fesi6.team1.study_group.domain.meetup.entity.Meetup;
 import com.fesi6.team1.study_group.domain.meetup.entity.MeetupLocation;
@@ -8,6 +9,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @AllArgsConstructor
@@ -19,11 +22,13 @@ public class WishlistMeetupResponseDTO {
     private MeetupLocation location;
     private String title;
     private String content;
+    private int minParticipants;
     private LocalDateTime recruitmentStartDate;
     private LocalDateTime recruitmentEndDate;
     private LocalDateTime meetingStartDate;
     private LocalDateTime meetingEndDate;
     private String thumbnail;
+    private final List<ParticipantDTO> participants;
 
     public WishlistMeetupResponseDTO(Meetup meetup) {
         meetupId = meetup.getId();
@@ -33,10 +38,17 @@ public class WishlistMeetupResponseDTO {
         location = meetup.getLocation();
         title = meetup.getTitle();
         content = meetup.getContent();
+        minParticipants = meetup.getMinParticipants();
         recruitmentStartDate = meetup.getCreatedAt();
         recruitmentEndDate = meetup.getRecruitmentEndDate();
         meetingStartDate = meetup.getMeetingStartDate();
         meetingEndDate = meetup.getMeetingEndDate();
         thumbnail = meetup.getThumbnail();
+        this.participants = meetup.getMeetupUsers().stream()
+                .map(meetupUser -> new ParticipantDTO(
+                        meetupUser.getUser().getId(),
+                        meetupUser.getUser().getProfileImg()
+                ))
+                .collect(Collectors.toList());
     }
 }
